6419e1182d9b748614f8b8413079d0287a0203de
Omkar Totade <omkar.totade@aeeiee.com>
2020-03-13 19:32:13 -0700

    Removed unnecessary JS

--- js/contactme-script.js
+++ js/contactme-script.js
@@ -1,19 +1,5 @@
 $(document).ready(function ()
 {
-  $(".navbar").hide();
-  $(".secondary-title-heading").hide();
-  $(".contact-text").hide();
-  $(".main-section-email").hide();
-  $(".contact-connect").hide();
-  $(".footer").hide();
-  $(".navbar").fadeIn(1750);
-  $(".secondary-title-heading").fadeIn(750);
-  $(".contact-text").fadeIn(1000);
-  $(".main-section-email").fadeIn(1250);
-  $(".contact-connect").fadeIn(1500);
-  $(".footer").fadeIn(2000);
-
-
   $("#email-button").click(function () {
 
     var name = $("#name").val();
