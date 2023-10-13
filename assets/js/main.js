907fe77100faeb52beca72245ab5298936d41fdd
omkartotade <totade.omkar1@gmail.com>
2023-10-13 02:18:29 -0400

    Another trivial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 var n = 1;
-function buttonClick() {
+document.getElementById('fun-btn').addEventListener('click', calcFun);
+function calcFun() {
 	if (n >= 0) {
 		document.getElementById('counter').innerHTML = n++;
 		if (n === 10) {
 			document.getElementById('fun-text').innerHTML = "That's enough for now!";
 			n = -1;
+			document.getElementById('fun-btn').removeEventListener('click', calcFun);
 		}
 	}
 }
